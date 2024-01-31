294ed2ed321727f7165616fe5a2cf1d1f0655950
MuminaB <barrowmuminatou@gmail.com>
2024-01-31 23:10:30 +0000

    square1

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,14 @@
+import Rectangle from './4-rectangle.js';
+export default class Square extends Rectangle {
+    constructor (size) {
+      super(size, size);
+    }
+  };
+/* const Rectangle =  require('./4-rectangle.js');
+module.exports = class Square extends Rectangle {
+    constructor (size) {
+      super(size, size);
+    }
+  };
+  */
+   
