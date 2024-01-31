db5c22ffb631ae06d8f9f97f888fa4a57a1083ed
MuminaB <barrowmuminatou@gmail.com>
2024-01-31 23:39:20 +0000

    require

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-import Rectangle from './4-rectangle.js';
-export default class Square extends Rectangle {
+const Rectangle =  require('./4-rectangle.js');
+module.exports = class Square extends Rectangle {
     constructor (size) {
       super(size, size);
     }
