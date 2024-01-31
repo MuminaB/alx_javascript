047a7c6d014db3dc059b9a73995d419e6aba8b5e
MuminaB <barrowmuminatou@gmail.com>
2024-01-31 23:24:03 +0000

    change

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 export default class Rectangle {
     constructor (w, h) {
-        if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+         }
       }
 }
- /* if (w > 0 && h > 0) {
-    this.width = w;
-    this.height = h;
- }  in the constructor  */ 
   
