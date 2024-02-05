9a5e6c1bf4140a5885d6c5bf14265f458930830d
MuminaB <barrowmuminatou@gmail.com>
2024-02-05 01:04:44 +0000

    -

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,5 +1,5 @@
 const req = require('request');
-request(process.argv[2], function (error, response, body) {
+req(process.argv[2], function (error, response, body) {
   if (!error) {
       const todos = JSON.parse(body);
       let completedtasks = {};
