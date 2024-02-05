ff1beae390a88c15c1ff27b2a5e6787ad026cc7a
MuminaB <barrowmuminatou@gmail.com>
2024-02-05 01:10:00 +0000

    tasks

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -2,14 +2,15 @@ const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const todos = JSON.parse(body);
-    let completedtasks = {};
+    let completed = {};
     todos.forEach((todo) => {
-      if (todo.completedtasks && completedtasks[todo.userId] === undefined) {
-        completedtasks[todo.userId] = 1;
-      } else if (todo.completedtasks) {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
         completed[todo.userId] += 1;
       }
     });
-    console.log(completedtasks);
+    console.log(completed);
   }
 });
+  
